754007a8727b201d8d377cd232871e6c241ddb7f
Elizabeth Purdom <epurdom@stat.berkeley.edu>
2021-11-16 14:09:15 -0800

    add colors to utils.R

--- R/utils.R
+++ R/utils.R
@@ -30,3 +30,79 @@
   samples <- do.call("rbind", samples)
   return(samples)
 }
+
+#' @rdname plottingColors
+#' @export
+bigPalette<-c(
+	'#E31A1C',
+	'#1F78B4',
+	'#33A02C',
+	'#FF7F00',
+	'#6A3D9A',
+	'#B15928',
+	'#A6CEE3',
+	'#bd18ea',
+	'cyan',
+	'#B2DF8A',
+	'#FB9A99',
+	"deeppink4",
+	'#00B3FFFF',
+	'#CAB2D6',
+	'#FFFF99',
+	'#05188a',
+	'#CCFF00FF',
+	'cornflowerblue',
+	'#f4cc03',
+	'black',
+	'blueviolet',
+	'#4d0776',
+	'maroon3',
+	'blue',
+#	'grey',
+	'#E5D8BD',
+	'cadetblue4',
+	'#e5a25a',
+	"lightblue1",
+	'#F781BF',
+	'#FC8D62',
+	'#8DA0CB',
+	'#E78AC3',
+	'green3',
+	'#E7298A',
+	'burlywood3',
+	'#A6D854',
+	"firebrick",
+	'#FFFFCC',
+	"mediumpurple",
+	'#1B9E77',
+	'#FFD92F',
+	'deepskyblue4',
+	"yellow3",
+	'#00FFB2FF',
+	'#FDBF6F',
+	'#FDCDAC',
+	"gold3",
+	'#F4CAE4',
+	'#E6F5C9',
+	'#FF00E6FF',
+	'#7570B3',
+	"goldenrod",
+	'#85848f',
+	"lightpink3",
+	"olivedrab",
+#	"plum",
+#	"lightskyblue3",
+#	"mediumturquoise",
+	'cadetblue3'
+)
+
+#' @importFrom grDevices colors
+.rcolors<-function(){
+	#so sure that setting seed doesn't mess up when installing package
+	set.seed(23589)
+	return(sample(colors()[-c(152:361)]))
+}
+
+#' @rdname plottingColors
+#' @export
+massivePalette<-unique(c(bigPalette,.rcolors()))
