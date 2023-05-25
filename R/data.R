66ae1705cee221ed0ea0fa40e9ca297bf066710f
William Torous <38775272+wtorousa@users.noreply.github.com>
2023-05-25 12:54:58 -0400

    Small type fixed in R/data.R

--- R/data.R
+++ R/data.R
@@ -8,7 +8,7 @@
 #' sample ID and phenotype for each cell, and the element `pca_embeddings`
 #' contains the first 50 PCs. These embeddings are provided by the authors of
 #' "Single-cell multi-omics analysis of the immune response in COVID-19"
-#' (Stephenson et al, 2021; doi: 10.1038/s41591-021-01329-2).
+#' (Stephenson et al., 2021; doi: 10.1038/s41591-021-01329-2).
 #' @format A list containing two data.frame: `metadata` (10000 x 2) and
 #' `pca_embeddings` (10000 x 50)
 NULL
