7e69bab557b425affe53e3323be233acf32273fb
wtorous <wgwtorous@gmail.com>
2023-10-02 19:49:06 -0700

    Update test cases to use a subset of 4 units

--- tests/testthat/test-cell.R
+++ tests/testthat/test-cell.R
@@ -6,9 +6,9 @@ test_that("gloscope warnings for small cell counts", {
 	expect_match(sample_size_warnings,"^The following samples have fewer than 500 cells.*",all = FALSE)
 	expect_match(sample_size_warnings,"^The following samples have fewer than the minimum of 50 cells*",all = FALSE)
 	# confirm the NA divergences are added last for samples with less than k cells
-	expect_equal(unname(colnames(na_div_matrix))[3],c("CV0234"))
-	expect_equal(unname(rownames(na_div_matrix))[3],c("CV0234"))
-	expect_equal(all(is.na(na_div_matrix[-3,-3])),FALSE)
+	expect_equal(unname(colnames(na_div_matrix))[4],c("CV0234"))
+	expect_equal(unname(rownames(na_div_matrix))[4],c("CV0234"))
+	expect_equal(all(is.na(na_div_matrix[-4,-4])),FALSE)
 	expect_equal(all(is.na(na_div_matrix["CV0234",])),TRUE)
 	expect_equal(all(is.na(na_div_matrix[,"CV0234"])),TRUE)
 	
@@ -20,19 +20,19 @@ test_that("gloscope warnings for small cell counts", {
 	# Case when no valid component numbers specified
 	num_component_warnings2 <- capture_warnings(small_gloscope <- gloscope(undersized_data_subset,undersized_metadata$sample_id,dens="GMM",num_components=c(50)))
 	expect_match(num_component_warnings2,"^Unable to fit a GMM with 50 components to a sample with 49 cells. Replacing those parameters with 48 components instead.*",all = FALSE)
-	expect_equal(dim(small_gloscope),c(3,3)) # Confirm a matrix is returned
+	expect_equal(dim(small_gloscope),c(4,4)) # Confirm a matrix is returned
 })
 
 test_that("gloscope works with KNN",{
   expect_silent(temp_knn<-gloscope(subsample_data_subset,subsample_metadata$sample_id,
       dens = "KNN", dist_mat = "KL",BPPARAM = BiocParallel::SerialParam(RNGseed=2)))
   #test dimensions
-  expect_equal(dim(temp_knn),c(3,3))
+  expect_equal(dim(temp_knn),c(4,4))
   #test distances the same as in the past
-  knn_expected_answer<-c(4.478759 ,3.203009, 1.520805) #answer got the first time
+  knn_expected_answer<-c(2.369535,4.478759,2.215369,3.203009,3.225507,1.520805) #answer got the first time
   expect_equal(round(temp_knn[upper.tri(temp_knn)],6),knn_expected_answer)
   #test diag zero
-  expect_equal(unname(diag(temp_knn)),rep(0,3))
+  expect_equal(unname(diag(temp_knn)),rep(0,4))
   #test row names/colnames
   expect_equal(unname(sort(colnames(temp_knn))),sort(unique(subsample_metadata$sample_id)))
   expect_equal(unname(sort(rownames(temp_knn))),sort(unique(subsample_metadata$sample_id)))
@@ -44,12 +44,12 @@ test_that("gloscope works with GMM",{
   expect_silent(temp_gmm<-gloscope(subsample_data_subset,subsample_metadata$sample_id,
       dens = "GMM", dist_mat = "KL",BPPARAM = BiocParallel::SerialParam(RNGseed=1)))
   #test dimensions
-  expect_equal(dim(temp_gmm),c(3,3))
+  expect_equal(dim(temp_gmm),c(4,4))
   #test distances the same as in the past
-  gmm_expected_answer<-round(c(12.69894, 10.18274, 10.45779),5) #answer got the first time
-  expect_equal(round(temp_gmm[upper.tri(temp_gmm)],5),gmm_expected_answer)
+  gmm_expected_answer <- c(14.220048,13.089617,18.073184,10.097857,11.156613,10.425697) #answer got the first time
+  expect_equal(round(temp_gmm[upper.tri(temp_gmm)],6),gmm_expected_answer)
   #test diag zero
-  expect_equal(unname(diag(temp_gmm)),rep(0,3))
+  expect_equal(unname(diag(temp_gmm)),rep(0,4))
   #test row names/colnames
   expect_equal(unname(sort(colnames(temp_gmm))),sort(unique(subsample_metadata$sample_id)))
   expect_equal(unname(sort(rownames(temp_gmm))),sort(unique(subsample_metadata$sample_id)))
@@ -166,7 +166,7 @@ test_that("Divergences are properly computed with GloScope inputs and KNN",{
   knn_density_list <- .calc_dens(embeddings_list, dens = "KNN")
   sample_pairs <- utils::combn(unique(sample_ids), 2)
   patient_pair_list <- lapply(seq_len(ncol(sample_pairs)), function(i) sample_pairs[,i])
-  w <- patient_pair_list[[3]] # pick one unit
+  w <- patient_pair_list[[6]] # pick one unit
   w_div_kl_expected <- 1.52
   w_div_kl <- .calc_kl(knn_density_list, embeddings_list, w[1], w[2], dens = "KNN")
   w_div_js_expected <- 0.04
@@ -183,7 +183,7 @@ test_that("Divergences are properly computed with GloScope inputs and GMM",{
   gmm_density_list <- .calc_dens(embeddings_list, dens = "GMM", BPPARAM = BiocParallel::SerialParam(RNGseed = 2))
   sample_pairs <- utils::combn(unique(sample_ids), 2)
   patient_pair_list <- lapply(seq_len(ncol(sample_pairs)), function(i) sample_pairs[,i])
-  w <- patient_pair_list[[1]] # pick one unit
+  w <- patient_pair_list[[2]] # pick one unit
   w_div_kl_expected <- 5.06
   set.seed(2)
   w_div_kl <- .calc_kl(gmm_density_list, embeddings_list, w[1], w[2], dens = "GMM")
@@ -194,9 +194,10 @@ test_that("Divergences are properly computed with GloScope inputs and GMM",{
   expect_equal(round(w_div_js,2),w_div_js_expected)
 })
 
-test_that("Divergens using cell type works properly",{
+test_that("Divergences using cell type works properly",{
   sample_ids <- subsample_metadata$sample_id
   sample_ids <- sample_ids[1:100]
   celltype <- subsample_metadata$cluster_id
   expect_error(cluster_distance(sample_ids, celltype),
-               regexp="Lengths of cell id and cell type are not equal!",fixed=TRUE)})
+               regexp="Lengths of cell id and cell type are not equal!",fixed=TRUE)
+})
